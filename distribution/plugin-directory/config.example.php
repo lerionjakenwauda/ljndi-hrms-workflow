8e7f60357b6ccd1719e05846c1540d3514c212df
Joseph Ebubechukuwu Nwauda <81398503+joseph724-Fk@users.noreply.github.com>
2026-07-30 14:40:31 +0100

    Add plugin directory configuration

---
+++ distribution/plugin-directory/config.example.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'site_name'     => 'LJNDI Plugins',
+    'company_name'  => 'Lerion Jake Nwauda Digital Innovations Ltd',
+    'base_url'      => 'https://lerionjakenwauda.com/plugins',
+    'support_url'   => 'https://lerionjakenwauda.com/contact',
+    'catalog_path'  => __DIR__ . '/storage/catalog',
+    'cache_seconds' => 300,
+);
